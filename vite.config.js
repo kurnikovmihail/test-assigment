43639ceb77eb5fa034de6e03add3e7e2c7dde1fc
Mihail <kurnikovmihail25@gmail.com>
2025-10-07 11:43:22 +0500

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Vue3-Test-Assignment/', // <-- именно так
+  base: '/test-assignment/', // <-- именно так
 })
